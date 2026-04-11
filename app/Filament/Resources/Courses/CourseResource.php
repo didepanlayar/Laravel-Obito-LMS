bebb1653883ee654f0c75c5ec0cf6ececdb4c70c
didepanlayar <albinmaulana@yahoo.com>
2026-04-11 15:17:50 +0700

    Feature: CRUD Course

--- app/Filament/Resources/Courses/CourseResource.php
+++ app/Filament/Resources/Courses/CourseResource.php
@@ -20,7 +20,7 @@ class CourseResource extends Resource
 {
     protected static ?string $model = Course::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::AcademicCap;
 
     public static function form(Schema $schema): Schema
     {
--- app/Filament/Resources/Courses/Schemas/CourseForm.php
+++ app/Filament/Resources/Courses/Schemas/CourseForm.php
@@ -2,6 +2,12 @@
 
 namespace App\Filament\Resources\Courses\Schemas;
 
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Fieldset;
 use Filament\Schemas\Schema;
 
 class CourseForm
@@ -10,7 +16,37 @@ class CourseForm
     {
         return $schema
             ->components([
-                //
+                Fieldset::make('Details')
+                    ->schema([
+                        TextInput::make('name')
+                            ->required()
+                            ->maxLength(255),
+                        FileUpload::make('thumbnail')
+                            ->required()
+                            ->image(),
+                    ]),
+                Fieldset::make('Additional')
+                    ->schema([
+                        Repeater::make('benefits')
+                            ->relationship('benefits')
+                            ->schema([
+                                TextInput::make('name')
+                                    ->required(),
+                            ]),
+                        Textarea::make('about')
+                            ->required(),
+                        Select::make('is_popular')
+                            ->required()
+                            ->options([
+                                true => 'Popular',
+                                false => 'Not Popular',
+                            ]),
+                        Select::make('category_id')
+                            ->required()
+                            ->relationship('category', 'name')
+                            ->searchable()
+                            ->preload(),
+                    ]),
             ]);
     }
 }
