8723614e228728d08d0707c90aef2421b7de6102
didepanlayar <albinmaulana@yahoo.com>
2026-04-14 18:13:01 +0700

    Feature: CRUD Transactions

--- app/Filament/Resources/Transactions/Schemas/TransactionForm.php
+++ app/Filament/Resources/Transactions/Schemas/TransactionForm.php
@@ -2,7 +2,15 @@
 
 namespace App\Filament\Resources\Transactions\Schemas;
 
+use App\Models\Pricing;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Grid;
+use Filament\Schemas\Components\Wizard;
+use Filament\Schemas\Components\Wizard\Step;
 use Filament\Schemas\Schema;
+use Illuminate\Support\Carbon;
 
 class TransactionForm
 {
@@ -10,7 +18,86 @@ class TransactionForm
     {
         return $schema
             ->components([
-                //
+                Wizard::make([
+                    Step::make('Product and Price')
+                        ->schema([
+                            Grid::make(2)
+                                ->schema([
+                                    Select::make('pricing_id')
+                                        ->required()
+                                        ->relationship('pricing', 'name')
+                                        ->searchable()
+                                        ->preload()
+                                        ->live()
+                                        ->afterStateUpdated(function ($state, callable $set) {
+                                            $pricing = Pricing::find($state);
+
+                                            $price = $pricing->price;
+                                            $duration = $pricing->duration;
+
+                                            $subTotal = $price * $state;
+                                            $totalPpn = $subTotal * 0.11;
+                                            $totalAmount = $subTotal + $totalPpn;
+
+                                            $set('total_tax_amount', $totalPpn);
+                                            $set('grand_total_amount', $totalAmount);
+                                            $set('sub_total_amount', $price);
+                                            $set('duration', $duration);
+                                        })
+                                        ->afterStateHydrated(function (callable $set, $state) {
+                                            $pricingId = $state;
+                                            if ($pricingId) {
+                                                $pricing = Pricing::find($pricingId);
+                                                $duration = $pricing->duration;
+                                                $set('duration', $duration);
+                                            }
+                                        }),
+                                    TextInput::make('duration')
+                                        ->required()
+                                        ->numeric()
+                                        ->readOnly()
+                                        ->prefix('Month'),
+                                ]),
+                            Grid::make(3)
+                                ->schema([
+                                    TextInput::make('sub_total_amount')
+                                        ->required()
+                                        ->numeric()
+                                        ->readOnly()
+                                        ->prefix('IDR'),
+                                    TextInput::make('total_tax_amount')
+                                        ->required()
+                                        ->numeric()
+                                        ->readOnly()
+                                        ->prefix('IDR'),
+                                    TextInput::make('grand_total_amount')
+                                        ->required()
+                                        ->numeric()
+                                        ->readOnly()
+                                        ->prefix('IDR')
+                                        ->helperText('Harga sudah termasuk PPN 11%'),
+                                ]),
+                            Grid::make(2)
+                                ->schema([
+                                    DatePicker::make('started_at')
+                                        ->required()
+                                        ->live()
+                                        ->afterStateUpdated(function ($state, callable $set, callable $get) {
+                                            $duration = $get('duration');
+                                            if ($state && $duration) {
+                                                $endedAt = Carbon::parse($state)->addMonth($duration);
+                                                $set('ended_at', $endedAt->format('Y-m-d'));
+                                            }
+                                        }),
+                                    DatePicker::make('ended_at')
+                                        ->required()
+                                        ->readOnly(),
+                                ])
+                        ])
+                ])
+                ->columnSpan('full')
+                ->columns(1)
+                ->skippable()
             ]);
     }
 }
--- app/Filament/Resources/Transactions/TransactionResource.php
+++ app/Filament/Resources/Transactions/TransactionResource.php
@@ -20,7 +20,7 @@ class TransactionResource extends Resource
 {
     protected static ?string $model = Transaction::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::ShoppingCart;
 
     public static function form(Schema $schema): Schema
     {
