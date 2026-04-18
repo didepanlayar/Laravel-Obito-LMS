36789f4b676215c54ddce20d6a1bb1d88f1ad423
didepanlayar <albinmaulana@yahoo.com>
2026-04-18 12:38:37 +0700

    Feature: Transaction Service Repository

---
+++ app/Repositories/TransactionRepositoryInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Transaction;
+use Illuminate\Support\Collection;
+
+interface TransactionRepositoryInterface
+{
+    public function findByBookingId(string $bookingId): ?Transaction;
+
+    public function create(array $data): Transaction;
+
+    public function getUserTransaction(int $id): Collection;
+}
---
+++ app/Services/TransactionService.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Pricing;
+use App\Models\Transaction;
+use App\Repositories\PricingRepositoryInterface;
+use App\Repositories\TransactionRepositoryInterface;
+use Illuminate\Support\Facades\Auth;
+
+class TransactionService
+{
+    protected $pricingRepository;
+    protected $transactionRepository;
+
+    public function __construct(PricingRepositoryInterface $pricingRepository, TransactionRepositoryInterface $transactionRepository)
+    {
+        $this->pricingRepository = $pricingRepository;
+        $this->transactionRepository = $transactionRepository;
+    }
+
+    public function prepareCheckout(Pricing $pricing)
+    {
+        $user = Auth::user();
+        $alreadySubscribed = $pricing->isSubscribedByUser($user->id);
+
+        $tax = 0.11;
+        $totalTaxAmount = $pricing->price * $tax;
+        $subTotalAmount = $pricing->price;
+        $grandTotalAmount = $subTotalAmount + $totalTaxAmount;
+
+        $startedAt = now();
+        $endedAt = $startedAt->copy()->addMonths($pricing->duration);
+
+        session()->put('pricing_id', $pricing->id);
+
+        return compact(
+            'user',
+            'alreadySubscribed',
+            'totalTaxAmount',
+            'subTotalAmount',
+            'grandTotalAmount',
+            'startedAt',
+            'endedAt'
+        );
+    }
+
+    public function getRecentPricing()
+    {
+        $pricingId = session()->get('pricing_id');
+
+        return $this->pricingRepository->findById($pricingId);
+    }
+
+    public function getUserTransactions()
+    {
+        $user = Auth::user();
+
+        if (!$user) {
+            return collect();
+        }
+
+        return $this->transactionRepository->getUserTransaction($user->id);
+    }
+}
