186269659f1222a43173afb46e5dcaa4144b7b50
didepanlayar <albinmaulana@yahoo.com>
2026-04-24 14:43:48 +0700

    Feature: Dashboard Controller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Transaction;
+use App\Services\TransactionService;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    //
+    protected $transactionService;
+
+    public function __construct(TransactionService $transactionService)
+    {
+        $this->transactionService = $transactionService;
+    }
+    
+    public function subscription()
+    {
+        $transactions = $this->transactionService->getUserTransactions();
+
+        // return to subscription
+    }
+    
+    public function subscriptionDetail(Transaction $transaction)
+    {
+        // return to subscription detail
+    }
 }
